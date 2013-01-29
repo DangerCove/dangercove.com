7e7a7b0273ad6e95b560e760fa74d3c571b55573
Boy van Amstel <boy@boyvanamstel.nl>
2013-01-29 12:56:37 +0100

    Switch to flatstrap

--- update_theme.sh
+++ update_theme.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+rm -rf sass
+rm -rf public
+rm -rf source/assets
+rm -rf source/javascripts
+rm -rf source/stylesheets
 bundle exec rake install["dangercove"] && bundle exec rake generate && bundle exec rake preview
